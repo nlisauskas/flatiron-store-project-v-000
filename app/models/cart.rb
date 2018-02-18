310642cf80c74f1cb33e472a6bb7a6ba5857f176
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-18 01:39:20 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,19 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
-  
+
+  def total
+    total = 0
+    self.items.each do |item|
+      total += item.price
+    end
+    total
+  end
+
+  def add_item(item)
+    
+  end
+
+
 
 end
