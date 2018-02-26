85b3e50b81be61386babffbfc9f18dfa951f8ce7
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-26 12:10:20 +0000

    automatically backed up by learn

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
+    def show
+        @cart = Cart.find(params[:id])
+    end
+
+    def checkout
+        cart = Cart.find params[:id]
+        cart.checkout
+       # cart.user.current_cart_id = nil
+        #require 'pry'; binding.pry
+        redirect_to cart_path(cart)
+    end
+
+    def index
+
+    end
 
 end
